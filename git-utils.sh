d11ba3196585649558fbefeca4b45a857f6466d4
jared.beach <jared.beach@capitalone.com>
2024-01-29 14:46:38 -0500

    git pull master no checkout
    and git get default branch

--- git-utils.sh
+++ git-utils.sh
@@ -1,3 +1,12 @@
 function get-mergeconflict-files() {
 	git diff --name-only --diff-filter=U --relative | cat
 }
+
+function pull-master-no-checkout() {
+	branch_name=$(get-git-default-branch)
+	git fetch origin $branch_name:$branch_name
+}
+
+function get-git-default-branch() {
+	git remote show origin | sed -n '/HEAD branch/s/.*: //p'
+}
