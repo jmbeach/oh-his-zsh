572747a9035f72105ea37029f315d5a16863c822
jared.beach <jared.beach@capitalone.com>
2024-11-06 14:16:04 -0500

    Remove aliases

--- aliases.sh
+++ aliases.sh
@@ -1,34 +1,8 @@
 next_pwd='~'
+
 function cd() {
     builtin cd "$@"
     next_pwd=$PWD
-}
-
-function rm() {
-    command rm "$@"
-    ls
-}
-
-function mkdir() {
-    command mkdir "$@"
-    ls
-}
-
-function mv() {
-    command mv "$@"
-    ls
-}
-
-function ls() {
-    if [[ $(uname -s) == "Darwin" ]]; then
-        LC_COLLATE=en_US.utf8 gls -C -w 3 --color=auto \
-            --group-directories-first \
-            "$@"
-    else
-        LC_COLLATE=en_US.utf8 command ls -C -w 3 --color=auto \
-            --group-directories-first \
-            "$@"
-    fi
 }
 
 function fd() {
@@ -60,7 +34,7 @@ function nextd() {
 }
 zle -N nextd
 function bindable-ls() {
-    ls
+    eza -l --no-user
     zle accept-line
 }
 zle -N bindable-ls
@@ -83,3 +57,5 @@ function qrg() {
 }
 
 alias cat='bat'
+alias ls='eza -l --no-user'
+alias sed='gsed'
